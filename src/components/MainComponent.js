e84f7ae9ecc9981b5027bdd6a9c5fe9937bb4cd0
Bang Nguyen <nguyenbahaibang@gmail.com>
2021-04-28 03:35:04 +0300

    Week 2: Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,18 +6,34 @@ import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 const Main = () => {
   const [dishes, setDishes] = useState(DISHES);
+  const [comments, setComments] = useState(COMMENTS);
+  const [promotions, setPromotions] = useState(PROMOTIONS);
+  const [leaders, setLeaders] = useState(LEADERS);
   const [selectedDish, setSelectedDish] = useState(null);
 
   return (
     <div>
       <Header />
       <Switch>
-        <Route path="/home" component={Home} />
+        <Route
+          path="/home"
+          component={() => (
+            <Home
+              dish={dishes.filter((dish) => dish.featured)[0]}
+              promotion={promotions.filter((promo) => promo.featured)[0]}
+              leader={leaders.filter((leader) => leader.featured)[0]}
+            />
+          )}
+        />
         <Route
           exact
           path="/menu"
@@ -25,6 +41,7 @@ const Main = () => {
             <Menu dishes={dishes} onDishSelect={setSelectedDish} />
           )}
         />
+        <Route exact path="/contactus" component={Contact} />
         <Redirect to="/home" />
       </Switch>
 
